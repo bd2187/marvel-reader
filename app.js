999edfee68913e9411598609227dd1b32fb40e60
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-07-16 19:17:59 -0700

    Connect to mongo db

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
+// Connect to Mongo DB
+mongoose
+  .connect(require("./config/db"))
+  .then(() => console.log("Connected to DB"))
+  .catch(() => console.log("Error connecting to DB"));
+
+// Set up bodyParser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
