549f4ed539c8e3aa84177c3cc00ed1aa5f2a8ccc
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-07-17 22:07:18 -0700

    Require user routes

--- app.js
+++ app.js
@@ -11,12 +11,16 @@ mongoose
   .catch(() => console.log("Error connecting to DB"));
 
 // Set up bodyParser middleware
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+// Routes
+const user = require("./routes/user");
+app.use("/user", user);
 
 const port = process.env.PORT || 5000;
 
