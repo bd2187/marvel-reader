19a6e799bdd6c62e29ffc2ad387a4bf8cdd07bfa
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-07-19 23:07:23 -0700

    Remove validateUserSignup fn to an external file. Require it

--- routes/user.js
+++ routes/user.js
@@ -4,36 +4,7 @@ const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
 
-const validateUserSignup = (userInput = {}) => {
-  const errors = {};
-
-  const { email, confirmEmail, password, confirmPassword } = userInput;
-
-  // Check for missing inputs
-  const missingFields = [];
-  Object.keys(userInput).forEach(prop => {
-    if (userInput[prop].length < 1) {
-      return missingFields.push(prop);
-    }
-  });
-
-  if (missingFields.length > 0) {
-    errors["missing_fields"] = missingFields;
-  }
-
-  // Compare Passwords & Emails
-  if (password.toLowerCase() !== confirmPassword.toLowerCase()) {
-    errors["password"] = "Passwords do not match.";
-  }
-
-  if (email.toLowerCase() !== confirmEmail.toLowerCase()) {
-    errors["email"] = "Emails do not match.";
-  }
-
-  errors["hasErrors"] = Object.keys(errors).length > 0 ? true : false;
-
-  return errors;
-};
+const { validateUserSignup } = require("../utils/validation");
 
 /**
  * Route: /user/signup
