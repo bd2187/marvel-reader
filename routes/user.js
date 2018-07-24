2a772915df61830aa15f8781e7a8735eb8250e10
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-07-23 19:46:26 -0700

    Create login route which will compare passwords and sign jwt token

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
+const key = require("../config/key");
 const User = require("../models/User");
 
 const { validateUserSignup } = require("../utils/validation");
@@ -49,6 +52,59 @@ router.post("/signup", (req, res) => {
         .catch(err => res.json({ error: true, msg: err }));
     });
   });
+});
+
+/**
+ * Route: /user/login
+ * Desc: Log in user
+ * Public Route
+ */
+router.post("/login", function(req, res) {
+  const { username, password } = req.body;
+
+  // Find user in DB
+  let promise = User.findOne({ username }).exec();
+
+  promise
+    .then(user => {
+      // If User doesn't exist, send error message
+      if (!user) {
+        return res.json({ status: "fail", msg: "user not found" });
+      }
+
+      // Compare password User's password from req.body to user.password from DB
+      bcrypt.compare(password, user.password).then(isMatch => {
+        // If password doesn't match, return error message
+        if (!isMatch) {
+          return res.json({ status: "fail", msg: "password incorrect" });
+        }
+
+        // Create payload
+        const payload = {
+          username: user.username,
+          email: user.email,
+          signUpDate: user.signUpDate
+        };
+
+        // Sign token with 1 hour expiration date
+        return jwt.sign(payload, key, { expiresIn: 3600000 }, (err, token) => {
+          if (err) {
+            return res.json({
+              status: "fail",
+              err
+            });
+          }
+
+          res.json({
+            status: "success",
+            token: `Bearer ${token}`
+          });
+        });
+      });
+    })
+    .catch(err => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
