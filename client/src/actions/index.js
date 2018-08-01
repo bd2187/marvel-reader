8a652896182c9eed560d6817ab86a93d837ec1e1
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-07-31 21:07:57 -0700

    Initial commit. Create logInUser action creator which will return sample data for now

---
+++ client/src/actions/index.js
@@ -0,0 +1,14 @@
+import { LOG_IN_USER } from "../constants";
+
+export const logInUser = userInput => {
+  // Temporary placeholder
+  return {
+    type: LOG_IN_USER,
+    user: {
+      username: "bd2187",
+      email: "mail@mail.com",
+      signUpDate: "7/31/18",
+      id: "idi3hasdfh13478k0"
+    }
+  };
+};
