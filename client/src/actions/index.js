58dcfa56a4656511b8a6df1a3039ecbf60b4c1a7
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-02 23:18:30 -0700

    Import axios and jwt-decode. Have logInUser return a function. When invoked, fire an ajax request using axios to log the username and password to the server. Decode the jwt token and dispatch the LOG_IN_USER with the userdata decoded from the token

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,14 +1,24 @@
 import { LOG_IN_USER } from "../constants";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
-export const logInUser = userInput => {
-  // Temporary placeholder
-  return {
-    type: LOG_IN_USER,
-    user: {
-      username: "bd2187",
-      email: "mail@mail.com",
-      signUpDate: "7/31/18",
-      id: "idi3hasdfh13478k0"
-    }
-  };
+export const logInUser = (username, password) => dispatch => {
+  axios
+    .post("/user/login", { username, password })
+    .then(data => {
+      var decoded = jwt_decode(data.data.token);
+      const { username, email, signUpDate, id } = decoded;
+      dispatch({
+        type: LOG_IN_USER,
+        user: {
+          username,
+          email,
+          signUpDate,
+          id
+        }
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 };
