160d3123496428aea354ffba63e9952f22b60eb7
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-03 22:42:01 -0700

    Save user's token in local storage. Invoke the setAuthorization to save the token in the auth header. Create the logOutUser action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,22 @@
 import { LOG_IN_USER } from "../constants";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
+import setAuthorization from "../utils/setAuthorization";
 
 export const logInUser = (username, password) => dispatch => {
   axios
     .post("/user/login", { username, password })
-    .then(data => {
-      var decoded = jwt_decode(data.data.token);
+    .then(res => {
+      // Save token in local storage
+      const { token } = res.data;
+      localStorage.setItem("token", token);
+
+      // Set auth header
+      setAuthorization(token);
+
+      // Decode JWT token
+      var decoded = jwt_decode(token);
+
       const { username, email, signUpDate, id } = decoded;
       dispatch({
         type: LOG_IN_USER,
@@ -20,5 +30,29 @@ export const logInUser = (username, password) => dispatch => {
     })
     .catch(function(err) {
       console.log(err);
+    });
+};
+
+export const logOutUser = (
+  username,
+  password,
+  confirmPassword,
+  email,
+  confirmEmail
+) => dispatch => {
+  axios
+    .post("/user/signup", {
+      username,
+      password,
+      confirmPassword,
+      email,
+      confirmEmail
+    })
+    .then(res => {
+      // redirect user to login page
+      return;
+    })
+    .catch(err => {
+      // do something with err
     });
 };
