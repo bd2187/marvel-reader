d853c8aae0e4f1d15326be87dc18a3ec28c52414
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-05 13:42:08 -0700

    Check for token in local storage. If token exists, decode it. If the expiration date has been reached, dispatch the logOutUser action. Otherwise, dispatch the loguserinfo action.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import jwt_decode from "jwt-decode";
 
 import { Provider } from "react-redux";
 import store from "./store";
+import { logUserInfo, logOutUser } from "./actions";
 
 import routes from "./config/routes";
+
+// Get token from local storage
+const token = localStorage.getItem("token");
+
+if (token) {
+  // If the token exists, decoded it
+  const decoded = jwt_decode(token);
+
+  // Get current time
+  const currentTime = Date.now() / 1000;
+  const tokenExp = decoded.exp;
+
+  /*
+    If the token expired, log the user out by dispatching the logOutUser action.
+    Otherwise, log the user in by dispatching logUserInfo(decoded)
+  */
+  tokenExp < currentTime
+    ? store.dispatch(logOutUser())
+    : store.dispatch(logUserInfo(decoded));
+}
 
 class App extends Component {
   render() {
