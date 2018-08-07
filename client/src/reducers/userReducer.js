89fefba1f1de9a9f2f13fcc2a000b6a6fb153a95
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-06 22:04:22 -0700

    Create cases for USER_SIGN_UP and USER_SIGN_UP_ERROR

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,17 +1,30 @@
-import { LOG_IN_USER, LOG_OUT_USER } from "../constants";
+import {
+  LOG_IN_USER,
+  LOG_OUT_USER,
+  USER_SIGN_UP_ERROR,
+  USER_SIGN_UP
+} from "../constants";
 
 const initialState = {
   isLoggedIn: false,
-  userData: {}
+  userData: {},
+  errors: {}
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOG_IN_USER:
-      return { ...state, isLoggedIn: true, userData: action.user };
+      return { ...state, isLoggedIn: true, userData: action.user, errors: {} };
 
     case LOG_OUT_USER:
-      return { ...state, isLoggedIn: false, userData: {} };
+      return { ...state, isLoggedIn: false, userData: {}, errors: {} };
+
+    case USER_SIGN_UP:
+      return { ...state, userData: { username: action.username }, errors: {} };
+
+    case USER_SIGN_UP_ERROR:
+      return { ...state, errors: { error: true, msg: action.msg } };
+
     default:
       return state;
   }
