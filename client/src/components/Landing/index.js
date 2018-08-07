c774b70d2a77701c37aebbe519105adf0eaa814c
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-06 18:47:36 -0700

    Extract LogIn form to its own component and import it to this file along with the Register component. Render both the LogIn and Register component inside of the landing component

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,60 +1,14 @@
 import React, { Component } from "react";
+import LogIn from "./LogIn";
+import Register from "./Register";
 
-class Landing extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: "",
-      password: ""
-    };
-
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onChange(e) {
-    const inputField = e.target.getAttribute("data-input");
-    const value = e.target.value;
-    this.setState({
-      [inputField]: value
-    });
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    const { username } = this.state;
-    const { password } = this.state;
-    const { logInUser } = this.props;
-
-    logInUser(username, password);
-  }
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            placeholder="Username"
-            data-input="username"
-            value={this.state.username}
-            onChange={this.onChange}
-          />
-
-          <input
-            type="password"
-            placeholder="Password"
-            data-input="password"
-            value={this.state.password}
-            onChange={this.onChange}
-          />
-
-          <input type="submit" value="Login" />
-        </form>
-      </div>
-    );
-  }
-}
+const Landing = props => {
+  return (
+    <div>
+      <LogIn logInUser={props.logInUser} />
+      <Register registerUser={props.registerUser} />
+    </div>
+  );
+};
 
 export default Landing;
