0a47871747f14461cbf392d09966fa9ef8cbf7c7
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-06 18:48:13 -0700

    Initial commit. Render LogIn form

---
+++ client/src/components/Landing/LogIn.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+class LogIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    const inputField = e.target.getAttribute("data-input");
+    const value = e.target.value;
+    this.setState({
+      [inputField]: value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const { username } = this.state;
+    const { password } = this.state;
+    const { logInUser } = this.props;
+
+    logInUser(username, password);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            data-input="username"
+            value={this.state.username}
+            onChange={this.onChange}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            data-input="password"
+            value={this.state.password}
+            onChange={this.onChange}
+          />
+
+          <input type="submit" value="Log In" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LogIn;
