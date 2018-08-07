ba1337aff9a4a1f5a8836991fca2637d91e050c6
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-06 22:05:29 -0700

    Add USER_SIGN_UP_ERROR and USER_SIGNUP_ constants

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,3 +1,7 @@
 export const LOG_IN_USER = "LOG_IN_USER";
 
 export const LOG_OUT_USER = "LOG_OUT_USER";
+
+export const USER_SIGN_UP_ERROR = "USER_SIGN_UP_ERROR";
+
+export const USER_SIGN_UP = "USER_SIGN_UP";
