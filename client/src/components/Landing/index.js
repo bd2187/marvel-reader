f72b7d03728e172d3319ebc46648bbb6770bc08d
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-08 22:53:20 -0700

    Add togle method to Landing component

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -2,13 +2,43 @@ import React, { Component } from "react";
 import LogIn from "./LogIn";
 import Register from "./Register";
 
-const Landing = props => {
-  return (
-    <div>
-      <LogIn logInUser={props.logInUser} />
-      <Register registerUser={props.registerUser} errors={props.errors} />
-    </div>
-  );
-};
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      login: true
+    };
+
+    this.toggleLogIn = this.toggleLogIn.bind(this);
+  }
+
+  toggleLogIn() {
+    this.setState(prevState => {
+      return { login: !prevState.login };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleLogIn}>
+          {this.state.login ? "Sign Up" : "Log In"}
+        </button>
+        {this.state.login ? (
+          <LogIn
+            logInUser={this.props.logInUser}
+            userData={this.props.userData}
+          />
+        ) : (
+          <Register
+            registerUser={this.props.registerUser}
+            errors={this.props.errors}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export default Landing;
