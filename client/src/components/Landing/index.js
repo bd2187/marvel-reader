b64f6aa3df777d7e9d139c638fb610f2d69a6754
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-15 07:00:44 -0700

    Add componentDidUpdate lifecycle method. If signUpSuccess is true, dispatch the logInUser action to log the user in the app (thus redirecting them to the dashboard)

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -19,6 +19,16 @@ class Landing extends Component {
     });
   }
 
+  componentDidUpdate() {
+    const { signUpSuccess } = this.props.userData;
+
+    if (signUpSuccess) {
+      const username = document.getElementById("signUpUsername").value;
+      const password = document.getElementById("signUpPassword").value;
+      this.props.logInUser(username, password);
+    }
+  }
+
   render() {
     return (
       <div>
@@ -26,12 +36,10 @@ class Landing extends Component {
           {this.state.login ? "Sign Up" : "Log In"}
         </button>
         {this.state.login ? (
-          <LogIn
-            logInUser={this.props.logInUser}
-            userData={this.props.userData}
-          />
+          <LogIn logInUser={this.props.logInUser} />
         ) : (
           <Register
+            logInUser={this.props.logInUser}
             registerUser={this.props.registerUser}
             errors={this.props.errors}
           />
