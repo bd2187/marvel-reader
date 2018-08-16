9b1e84321b9cceb48969dc95a0b4a43d78c82400
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-16 07:17:38 -0700

    Change the value of the errors variable from an object to an array. And for each error, push it into the array. Return the array

--- utils/validation.js
+++ utils/validation.js
@@ -1,30 +1,44 @@
 const validateUserSignup = (userInput = {}) => {
-  const errors = {};
+  let errors = [];
 
   const { email, confirmEmail, password, confirmPassword } = userInput;
 
   // Check for missing inputs
-  const missingFields = [];
+
   Object.keys(userInput).forEach(prop => {
     if (userInput[prop].length < 1) {
-      return missingFields.push(prop);
+      // return missingFields.push(prop);
+      switch (prop) {
+        case "username":
+          errors.push("Username is required.");
+          break;
+        case "password":
+          errors.push("Password is required.");
+          break;
+        case "confirmPassword":
+          errors.push("Please confirm your password");
+          break;
+        case "email":
+          errors.push("Email is required.");
+          break;
+        case "confirmEmail":
+          errors.push("Please confirm your email");
+          break;
+        default:
+          errors.push("One or more fields is missing");
+      }
     }
   });
 
-  if (missingFields.length > 0) {
-    errors["missing_fields"] = missingFields;
-  }
-
   // Compare Passwords & Emails
   if (password.toLowerCase() !== confirmPassword.toLowerCase()) {
-    errors["password"] = "Passwords do not match.";
+    // errors["password"] = "Passwords do not match.";
+    errors.push("Passwords do not match.");
   }
 
   if (email.toLowerCase() !== confirmEmail.toLowerCase()) {
-    errors["email"] = "Emails do not match.";
+    errors.push("Emails do not match.");
   }
-
-  errors["hasErrors"] = Object.keys(errors).length > 0 ? true : false;
 
   return errors;
 };
