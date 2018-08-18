c167594419059c8aa4c8c7531126072c1182bac0
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-18 09:47:26 -0700

    Import PrivateRouteContainer and DashboardContainer.

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import PrivateRouteContainer from "../containers/common/PrivateRouteContainer";
 import LandingContainer from "../containers/LandingContainer";
+import DashboardContainer from "../containers/DashboardContainer";
 
 const routes = (
   <BrowserRouter>
-    <Route exact path="/" component={LandingContainer} />
+    <div>
+      <Route exact path="/" component={LandingContainer} />
+      <PrivateRouteContainer path="/dashboard" component={DashboardContainer} />
+    </div>
   </BrowserRouter>
 );
 
