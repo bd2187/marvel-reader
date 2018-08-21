9c0fd93faeec3121fe88dd2c55aba76d085bb4ca
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-21 07:29:39 -0700

    Import ComicsContainer. Create a route for it under /comics

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import PrivateRouteContainer from "../containers/common/PrivateRouteContainer";
 import LandingContainer from "../containers/LandingContainer";
 import DashboardContainer from "../containers/DashboardContainer";
+import ComicsContainer from "../containers/ComicsContainer";
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route exact path="/" component={LandingContainer} />
+      <Route exact path="/comics" component={ComicsContainer} />
       <PrivateRouteContainer path="/dashboard" component={DashboardContainer} />
-    </div>
+    </Switch>
   </BrowserRouter>
 );
 
