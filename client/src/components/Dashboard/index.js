bc0812feba74b62c014a3fa6ad48e64edbb93335
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-21 07:30:47 -0700

    Import Favorites component and render it twice-- once for fave characters and a second time for fave comics. Render two links for comics and characters

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Favorites from "./Favorites";
 
 const Dashboard = props => {
-  console.log(props);
-  return <h1>Dashboard</h1>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Favorites
+        category="Characters"
+        collection={props.profile.favoriteCharacters}
+      />
+      <Favorites category="Comics" collection={props.profile.favoriteComics} />
+
+      <Link to="/comics">More Comics</Link>
+      <Link to="/characters">More Characters</Link>
+    </div>
+  );
 };
 
 export default Dashboard;
