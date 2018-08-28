9a8bd6e214ed9fd4a91d27029e1206f3b6f28d25
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-08-28 07:47:42 -0700

    Import main.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./styles/main.css";
 import "./styles/normalize.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
