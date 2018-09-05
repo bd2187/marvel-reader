de0903249662f78d81b6d7e1a4f456188d9101d7
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-09-05 07:43:01 -0700

    Initial commit. Render Sidenav

---
+++ client/src/components/Navbar/SideNav.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css";
+
+const SideNav = props => {
+  const { navbarOpen, user, toggleNav, logOut } = props;
+  return (
+    <div
+      id={styles.sideNav}
+      className={navbarOpen ? styles["sidenav-open"] : styles["sidenav-close"]}
+    >
+      {user.isLoggedIn ? (
+        <Link
+          className={styles["sidenav-link"]}
+          to="/dashboard"
+          onClick={toggleNav}
+        >
+          Profile
+        </Link>
+      ) : (
+        <Link className={styles["sidenav-link"]} to="/" onClick={toggleNav}>
+          Sign In
+        </Link>
+      )}
+
+      <Link className={styles["sidenav-link"]} to="/comics" onClick={toggleNav}>
+        Comics
+      </Link>
+      <Link
+        className={styles["sidenav-link"]}
+        to="/characters"
+        onClick={toggleNav}
+      >
+        Characters
+      </Link>
+      <span className={styles["sidenav-link"]} onClick={logOut}>
+        LogOut
+      </span>
+    </div>
+  );
+};
+
+export default SideNav;
