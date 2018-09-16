ae57dbeb40c1387c2287f7c6a8ea6b2af74536eb
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-09-16 16:16:11 -0700

    Render Modal component if modalOpen is set to true. Pass openModal and closeModal functions as click events to the Modal component

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -1,24 +1,38 @@
 import React, { Component } from "react";
-import Thumbnail from "./Thumbnail";
 import styles from "./Grid.module.css";
+
+import Thumbnail from "./Thumbnail";
+import Modal from "./Modal";
 
 class Grid extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      modalOpen: false
+      modalOpen: false,
+      activeData: {}
     };
 
     this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
-  openModal() {
-    this.setState(() => ({ modalOpen: true }));
+  openModal(data) {
+    this.setState(() => ({ modalOpen: true, activeData: data }));
+  }
+
+  closeModal(evt) {
+    // todo: add condition for evt
+    [...evt.target.classList].forEach(name => {
+      return name.indexOf("modal-overlay") !== -1
+        ? this.setState(() => ({ modalOpen: false, activeData: {} }))
+        : null;
+    });
   }
 
   render() {
     const { content, loading, title } = this.props;
+    const { modalOpen, activeData } = this.state;
     return (
       <div className={styles.wrap}>
         {content.length === 0 && !loading ? <h1>No {title} found</h1> : null}
@@ -31,10 +45,14 @@ class Grid extends Component {
                 title={item.title || item.name}
                 thumbnail={item.thumbnail}
                 openModal={this.openModal}
+                data={item}
               />
             );
           })}
         </ul>
+        {modalOpen ? (
+          <Modal content={activeData} closeModal={this.closeModal} />
+        ) : null}
         {loading ? <div className={styles.loader} /> : null}
       </div>
     );
