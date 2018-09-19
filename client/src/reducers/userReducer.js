069a00a44e147f457bf3cec7edbdd0d87530f3ce
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-09-18 19:27:47 -0700

    Add loading case to reducer

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -3,11 +3,13 @@ import {
   LOG_OUT_USER,
   USER_SIGN_UP_ERROR,
   USER_SIGN_UP,
-  USER_LOG_IN_ERROR
+  USER_LOG_IN_ERROR,
+  USER_SIGN_UP_LOADING
 } from "../constants";
 
 const initialState = {
   isLoggedIn: false,
+  loading: false,
   userData: {},
   errors: {}
 };
@@ -20,18 +22,30 @@ const userReducer = (state = initialState, action) => {
     case LOG_OUT_USER:
       return { ...state, isLoggedIn: false, userData: {}, errors: {} };
 
+    case USER_SIGN_UP_LOADING:
+      return { ...state, loading: action.loading };
+
     case USER_SIGN_UP:
       return {
         ...state,
         userData: { username: action.username, signUpSuccess: true },
-        errors: {}
+        errors: {},
+        loading: false
       };
 
     case USER_SIGN_UP_ERROR:
-      return { ...state, errors: { error: true, msg: action.msg } };
+      return {
+        ...state,
+        errors: { error: true, msg: action.msg },
+        loading: false
+      };
 
     case USER_LOG_IN_ERROR:
-      return { ...state, errors: { error: true, msg: action.msg } };
+      return {
+        ...state,
+        errors: { error: true, msg: action.msg },
+        loading: false
+      };
 
     default:
       return state;
