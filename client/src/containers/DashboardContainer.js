7b6396739a803fb060d6e98cb157af3b4bcc5411
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-10-07 12:42:32 -0700

    Import the getAllComics aciton creator and map it to the container's props object

--- client/src/containers/DashboardContainer.js
+++ client/src/containers/DashboardContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
+import { getAllComics } from "../actions";
 
 import Dashboard from "../components/Dashboard";
 
 class DashboardContainer extends Component {
+  componentDidMount() {
+    this.props.fetchComics();
+  }
   render() {
     const { user, profile } = this.props;
     return <Dashboard user={user} profile={profile} />;
@@ -18,4 +22,15 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(DashboardContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchComics: function() {
+      dispatch(getAllComics());
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DashboardContainer);
