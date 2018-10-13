f57fa32562a5e8af7842bc30d5da09756382e5a7
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-10-13 11:46:43 -0700

    Initial commit. Declare Months array and zeroCheck function

---
+++ client/src/utils/dates.js
@@ -0,0 +1,16 @@
+export const Months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+export const zeroCheck = num => (num >= 10 ? num : "0" + num);
