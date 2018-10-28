53109fc42835fc7d09d747cbce8b154502d681e3
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-10-28 12:38:40 -0700

    Add logic for displaying favorites

--- client/src/components/Dashboard/Favorites.js
+++ client/src/components/Dashboard/Favorites.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Favorites = props => {
   const { category, collection } = props;
@@ -8,10 +9,34 @@ const Favorites = props => {
       {collection && Object.keys(collection).length === 0 ? (
         <p>You have no favorite {category.toLowerCase()} at the moment</p>
       ) : (
-        <h1>Favorite {category}</h1>
-      )}
+        <div>
+          <h1>Favorite {category}</h1>
 
-      {/* display collection here */}
+          <ul>
+            {collection.map(function(item) {
+              let linkToFave;
+              let faveName;
+
+              if (category.toLowerCase() === "characters") {
+                linkToFave = `/characters/${item.characterID}`;
+                faveName = item.name;
+              } else if (category.toLowerCase() === "comics") {
+                linkToFave = `/comics/${item.comicID}`;
+                faveName = item.title;
+              }
+
+              return (
+                <li key={item.characterID || item.comicID}>
+                  <h3>{faveName}</h3>
+                  <Link to={linkToFave}>
+                    <img src={item.thumbnail} alt={faveName} />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
