667d687e3c6f6c5fa645962efb3db2827a32c540
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-10-31 19:09:33 -0700

    Import styles

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./Favorites.module.css";
 
 import Favorites from "./Favorites";
 
 const Dashboard = props => {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div style={{ padding: "75px 0" }}>
       <Favorites
         category="Characters"
         collection={props.profile.favoriteCharacters}
       />
       <Favorites category="Comics" collection={props.profile.favoriteComics} />
 
-      <Link to="/comics">More Comics</Link>
-      <Link to="/characters">More Characters</Link>
+      <div className={styles["more-links-container"]}>
+        <Link to="/comics">More Comics</Link>
+        <Link to="/characters">More Characters</Link>
+      </div>
     </div>
   );
 };
