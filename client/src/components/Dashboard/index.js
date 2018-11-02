f4ce0a96f452d56ab6e0a8be6487af484a8445a2
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-11-01 19:08:09 -0700

    Pass the delete props for comics and characters to the Favorites component

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -5,13 +5,21 @@ import styles from "./Favorites.module.css";
 import Favorites from "./Favorites";
 
 const Dashboard = props => {
+  const { user, profile, deleteFavoriteComic, deleteFavoriteCharacter } = props;
+
+  // Todo: render user data
   return (
     <div style={{ padding: "75px 0" }}>
       <Favorites
         category="Characters"
-        collection={props.profile.favoriteCharacters}
+        collection={profile.favoriteCharacters}
+        deleteFromFaveList={deleteFavoriteCharacter}
       />
-      <Favorites category="Comics" collection={props.profile.favoriteComics} />
+      <Favorites
+        category="Comics"
+        collection={profile.favoriteComics}
+        deleteFromFaveList={deleteFavoriteComic}
+      />
 
       <div className={styles["more-links-container"]}>
         <Link to="/comics">More Comics</Link>
