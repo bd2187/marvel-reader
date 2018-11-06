3c9412c50665db27eddbc9f3eefeac99bb485913
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-11-05 18:45:26 -0800

    Display user data

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Favorites.module.css";
+import { Months } from "../../utils/dates";
 
 import Favorites from "./Favorites";
+
+const displayDate = signUpDate => {
+  var d = new Date(signUpDate);
+  var month = d.getUTCMonth();
+  var date = d.getUTCDate();
+  var year = d.getFullYear();
+
+  return `${Months[month]} ${date}, ${year}`;
+};
 
 const Dashboard = props => {
   const { user, profile, deleteFavoriteComic, deleteFavoriteCharacter } = props;
@@ -10,6 +20,11 @@ const Dashboard = props => {
   // Todo: render user data
   return (
     <div style={{ padding: "75px 0" }}>
+      <div className={styles["user-info-container"]}>
+        <h3>Username: {user.userData.username}</h3>
+        <h3>Email: {user.userData.email}</h3>
+        <p>Member since: {displayDate(user.userData.signUpDate)}</p>
+      </div>
       <Favorites
         category="Characters"
         collection={profile.favoriteCharacters}
